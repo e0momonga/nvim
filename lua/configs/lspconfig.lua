5911c666192b0309b230e026df5aafa1e3d2978a
Visha765 <lewis.ap75@gmail.com>
2025-10-08 01:07:51 +0900

    fix: lspconfig deprecated errorに対応

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,28 +1,9 @@
-local configs = require("nvchad.configs.lspconfig")
-
-local on_attach = configs.on_attach
-local on_init = configs.on_init
-local capabilities = configs.capabilities
-
 -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "rust_analyzer", "gopls", "ruby_lsp", "terraformls" }
+local servers = { "html", "cssls", "clangd", "rust_analyzer", "gopls", "ruby_lsp", "terraformls", "ts_ls" }
 
--- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+  vim.lsp.enable(lsp)
 end
-
--- typescript
-lspconfig.ts_ls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
 
 -- dart
 -- require("lspconfig").dartls.setup({
