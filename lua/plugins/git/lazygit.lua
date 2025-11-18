5628fdcd925b59e20b6b6a016579c251cd12a8b4
e0momonga <lewis.ap75@gmail.com>
2025-11-18 11:43:44 +0900

    feat: lazygit

--- lua/plugins/git/lazygit.lua
+++ lua/plugins/git/lazygit.lua
@@ -6,5 +6,15 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    config = function()
+      -- Get the Neovim server address and pass it to lazygit
+      vim.g.lazygit_floating_window_use_plenary = 0
+
+      -- Set NVIM environment variable for child processes
+      local nvim_server = vim.v.servername
+      if nvim_server then
+        vim.env.NVIM = nvim_server
+      end
+    end,
   },
 }
