68f51ce129f076cc2da410b9c671a2e3ce49e057
ez0momonga <keisuke.monma@techouse.jp>
2025-11-19 12:31:55 +0900

    Revert "fix: 暫定でruby_lspを使えるようにした"
    This reverts commit 6e49e7e2fb7a16bdd5ab0205ec50e5c1988a8edf.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,13 @@
--- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-local servers = { "html", "cssls", "clangd", "rust_analyzer", "gopls", "ruby_lsp", "terraformls", "ts_ls", "biome", "marksman" }
+-- ========================================
+-- LSP サーバー管理
+-- ========================================
+--
+-- LSP サーバーは mason-lspconfig.nvim (lua/plugins/mason.lua) で自動管理されています。
+-- 新しい LSP を追加する場合は lua/data/mason-tools.lua を編集してください。
+--
+
+-- mason-lspconfig に完全に任せるため、空リストに設定
+local servers = {}
 
 for _, lsp in ipairs(servers) do
   vim.lsp.enable(lsp)
