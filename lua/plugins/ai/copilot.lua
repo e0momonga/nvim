a970ec22ee8c99b8106f81c054c6276ab925ed9f
e0momonga <lewis.ap75@gmail.com>
2026-01-08 00:28:49 +0900

    feat: 補完の確定bindを変更

--- lua/plugins/ai/copilot.lua
+++ lua/plugins/ai/copilot.lua
@@ -25,7 +25,7 @@ return {
           auto_trigger = true,
           debounce = 75,
           keymap = {
-            accept = "<C-k>",
+            accept = "<M-k>",
             accept_word = false,
             accept_line = false,
             next = "<M-]>",
