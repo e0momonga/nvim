cf5c21bb8b314ba466bfda8ebc92d11628738403
ez0momonga <keisuke.monma@techouse.jp>
2026-01-10 12:20:16 +0900

    fix: Enter mapping

--- lua/plugins/editing/cmp.lua
+++ lua/plugins/editing/cmp.lua
@@ -8,7 +8,7 @@ return {
           cmp.abort() -- 候補を閉じる
         end
         fallback() -- 改行
-      end),
+      end, {"i"}), -- 挿入モードのみに制限
       ["<C-k>"] = cmp.mapping.confirm { select = true }, -- 確定
       ["<Tab>"] = cmp.mapping.select_next_item(), -- 次の候補
       ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- 前の候補
